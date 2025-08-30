acbb0609277643b59c3dbd3ba23ec03b503bd534
balinor75 <balinor75@gmail.com>
2025-08-30 09:39:31 +0000

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true,
     port: 3000,
-    allowedHosts: ["swlncd-3000.csb.app"],
+    //allowedHosts: ["swlncd-3000.csb.app"],
   },
 });
