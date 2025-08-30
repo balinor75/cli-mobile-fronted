58a214b06bb9dae143df84fecdab53c1ee752ade
balinor75 <balinor75@gmail.com>
2025-08-30 10:48:29 +0200

    Create UploadOrders.vue

---
+++ src/views/UploadOrders.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="upload">
+    <h3>Upload Orders File</h3>
+    <input type="file" @change="handleFile" />
+    <button @click="upload">Upload</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { uploadOrders } from '../services/api';
+
+const file = ref(null);
+
+// Salva il file selezionato
+const handleFile = (event) => {
+  file.value = event.target.files[0];
+};
+
+// Invia il file e scarica il risultato
+const upload = async () => {
+  if (!file.value) return alert('Select a file first');
+  const blob = await uploadOrders(file.value);
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'orders_result.xlsx';
+  a.click();
+};
+</script>
+
+<style scoped>
+.upload {
+  margin: 1rem;
+}
+</style>
