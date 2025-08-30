b276b4d6fb0fdf5448d4e3795d1d0a3ec2746879
balinor75 <balinor75@gmail.com>
2025-08-30 10:52:24 +0200

    Create UploadView.vue

---
+++ src/components/UploadView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Upload Files</h2>
+    <UploadSales />
+    <UploadOrders />
+  </div>
+</template>
+
+<script setup>
+import UploadSales from '../components/UploadSales.vue';
+import UploadOrders from '../components/UploadOrders.vue';
+</script>
+
+<style scoped>
+div {
+  padding: 1rem;
+}
+</style>
